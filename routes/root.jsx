63c5283bb11983e941bd1b8a7834d34a9baf6239
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-15 16:31:21 +0530

    adding react router

---
+++ routes/root.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Root() {
+  return (
+    <>
+        <header>
+      <div className='flex  justify-center flex-wrap gap-10 items-center'>
+        <h2 className='text-2xl'>
+          DressCart 
+        </h2>
+        <img className='cartIcon' src="/media/cart.svg"  alt="" />
+      </div>
+    </header>
+    
+    <footer>
+
+    </footer>
+
+    </>
+
+  )
+}
+
+export default Root
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Root from '../routes/root.jsx';
+import ErrorPage from './error-page.jsx';
+import Home from '../routes/home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage/>,
+    children : [
+      {
+        path: "/",
+        element: <Home/>
+      },
+    ]
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
