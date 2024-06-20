2e4e584b756034b036221b7e354b877f457f943b
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-20 17:10:23 +0530

    Create productSingle.jsx

---
+++ routes/productRelated/productSingle.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link, useLoaderData,} from 'react-router-dom'
+
+
+
+export async function loader({params}) {
+  //code for fetching product details
+  const response = await fetch(`http://localhost:3000/products/${params.productId}`)
+  const product = await response.json()
+
+  return {product};
+}
+
+function ProductSingle() {
+  const {product} = useLoaderData()
+  console.log(product)
+  return (
+
+<>    
+    <article className="  container max-w-2xl mx-auto productList flex flex-col">
+
+
+
+
+    <div className="productDetails mt-10 flex flex-col items-start mx-auto gap-2">
+    <img className='max-h-fit max-w-80   '
+      src={product.mainImage}
+       alt=""
+    />
+
+    <h3 className="h6">{product.brand}</h3>
+    <p>{product.description}</p>
+
+
+      <div>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>☆ </span>
+      </div>
+      <div className="priceAndButton flex flex-row gap-5">
+        <span className="p">$ {product.price}</span>
+        <button className="border px-2 rounded bg-gray-800 text-white button buttonPrimary">Add to cart</button>
+      </div>
+    </div>
+  </article>
+  </>
+
+   
+  )
+}
+
+export default ProductSingle
+
+
