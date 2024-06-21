8e5e6db8941298a15e35768a638f7f9ea4c9450b
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-22 00:25:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,9 @@ import Categories,{loader as categoriesLoader} from '../routes/productRelated/ca
 import Category, {loader as categoryLoader} from '../routes/productRelated/category.jsx';
 import ProductsAll, {loader as allLoader} from '../routes/productRelated/productsAll.jsx';
 import ProductSingle, {loader as singleLoader} from '../routes/productRelated/productSingle.jsx';
+import Cart from '../routes/productRelated/cart.jsx';
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter([
   {
@@ -44,8 +47,12 @@ const router = createBrowserRouter([
         path: "/products/:productId",
         element: <ProductSingle/>,
         loader: singleLoader
-      }
+      },
+      {
+        path: "/cart",
+        element: <Cart/>
 
+      }
     ]
   },
 
@@ -53,6 +60,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
